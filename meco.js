8893cb4f7ead17bca070d9906e3dd4e73d33553c
Damo0007 <106745333+Damo0007@users.noreply.github.com>
2023-01-22 09:59:59 +0100

    update

---
+++ meco.js
@@ -0,0 +1,31 @@
+// document.getElementById('name').innerHTML = 'i am here'
+// document.getElementById('namee').innerHTML = 'i am here'
+
+// document.querySelector('#name').innerHTML = 'i am here'
+
+var a = 2
+var b =3
+var c= a + b
+
+console.log(c);
+
+document.getElementById('name').innerHTML = c
+
+
+var d =10
+var e =5
+var f= d + e
+console.log(f)
+document.getElementById('dave').innerHTML = f
+
+var g=50
+var r=10
+var h=g+r
+document.getElementById('grace').innerHTML= 'i am ' + h + ' years old'
+
+console.log('mind' + 'set=');
+
+var m=20
+var n=10
+var o=m+n
+document.getElementById('hee').innerHTML = o
