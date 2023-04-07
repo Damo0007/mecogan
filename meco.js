bbdb2a766a22d2e2833319e9abd5520be8601e55
Damo0007 <106745333+Damo0007@users.noreply.github.com>
2023-04-07 11:55:06 +0100

    final

--- meco.js
+++ meco.js
@@ -3,29 +3,40 @@
 
 // document.querySelector('#name').innerHTML = 'i am here'
 
-var a = 2
-var b =3
-var c= a + b
+// var a = 2
+// var b =3
+// var c= a + b
 
-console.log(c);
+// console.log(c);
 
-document.getElementById('name').innerHTML = c
+// document.getElementById('name').innerHTML = c
 
 
-var d =10
-var e =5
-var f= d + e
-console.log(f)
-document.getElementById('dave').innerHTML = f
+// var d =10
+// var e =5
+// var f= d + e
+// console.log(f)
+// document.getElementById('dave').innerHTML = f
 
-var g=50
-var r=10
-var h=g+r
-document.getElementById('grace').innerHTML= 'i am ' + h + ' years old'
+// var g=50
+// var r=10
+// var h=g+r
+// document.getElementById('grace').innerHTML= 'i am ' + h + ' years old'
 
-console.log('mind' + 'set=');
+// console.log('mind' + 'set=');
 
-var m=20
-var n=10
-var o=m+n
-document.getElementById('hee').innerHTML = o
+// var m=20
+// var n=10
+// var o=m+n
+// document.getElementById('hee').innerHTML = o
+
+function nav(){
+    document.getElementById('exit').style.display="block"
+    document.querySelector('#nav').style.display="block"
+    document.getElementById('menu-icon').style.display="none"
+}
+function hide(){
+    document.getElementById('exit').style.display="none"
+    document.getElementById('menu-icon').style.display="block"
+    document.querySelector('#nav').style.display="none"
+}
